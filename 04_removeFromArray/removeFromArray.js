b8c6f3822ed1f4d10f95b30d42ea5ec86c5f0459
Ishak <ishakkovacevic@gmail.com>
2022-09-12 15:51:12 -0500

    v1.3.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arg1, arg2, arg3) {
+     for (let number of arg1){
+        if (arg2 === number && arg3 === undefined){
+          let remove = arg1.indexOf(number);
+          arg1.splice(remove,1);
+          return arg1;
+        } else if (arg3 === number && arg2 > 0){
+            remove = arg1.indexOf(number);
+            arg1.splice(remove,2);
+            return arg1;
+        }
+     };
 };
 
 // Do not edit below this line
