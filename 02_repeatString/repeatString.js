28c3517716f27982d9fd0f10238d100063042edb
Ishak <ishakkovacevic@gmail.com>
2022-09-12 14:51:56 -0500

    v1.2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
 
+const repeatString = function(arg1, arg2) {
+    if (arg2 === 0){
+        let string = ""
+        return string;
+    } else if (arg2 < 0){
+        string = "ERROR";
+        return string;
+    } else if(arg1 === "") {
+        string = arg1;
+        return string;
+    } else {
+        string = arg1;
+        for (let i=0; i < arg2 - 1; i++){
+        string = string + arg1;
+    };
+        return string;
+    };
+    
 };
 
 // Do not edit below this line
